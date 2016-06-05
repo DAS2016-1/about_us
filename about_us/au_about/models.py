40048bc09a5030fcbb4d738ff41216abd6147dbe
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-06-05 11:57:17 -0300

    Creating a model to About

--- about_us/au_about/models.py
+++ about_us/au_about/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class About(models.Model):
+    comment = models.TextField()
+    positive_votes = models.IntegerField()
+    negative_votes = models.IntegerField()
