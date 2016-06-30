be205492199f3a15057c63fb8ecee3c55f68a351
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-06-30 15:31:46 -0300

    Adding a field in the models About
    Signed-off-by: tiagoassuncao <thiagoribeironiquel@hotmail.com>
    
    Adding a migrate
    
    Signed-off-by: tiagoassuncao <thiagoribeironiquel@hotmail.com>

--- about_us/au_auth/models.py
+++ about_us/au_auth/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     int_number = models.IntegerField()
+    unread_abouts = models.IntegerField(default=0, null=True)
 
     def __str__(self):
         return str(self.user)
